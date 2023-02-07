b4280a4326e83ac6073bcf4132f7633eb70f0fef
Fanger53 <fanger53@gmail.com>
2023-02-07 01:27:19 -0500

    add basic layout and styles to about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import './about.scss';
+import {Button, Col, Container, Row} from 'react-bootstrap';
+
 
 const About = () => {
   return (
-    <section  className='border about__section'>
+    <Container  className='border about '>
       <h2>About Me</h2>
       <p>Get to Know Me</p>
-      <div className='d-flex'>
-        <h4>who am I?</h4>
-        <img src="" alt="imagen about Me" />
+      <div className='d-flex justify-content-center '>
+      <Row className=' about__section '>
+        <Col sm={12} md={6}>
+          <img src="" alt="imagen about Me" />
+        </Col>  
+        <Col sm={12} md={6}>
+          <h4>who am I?</h4>
+          <p>
+            I’m a Full-Stack Software Developer from Colombia with experience in Html, Scss, JavaScript, Ruby, Ruby on Rails,React and more technologies.
+            I started my professional career in sales, but always in the background learning about the world of programming until 2019 now I dedicate myself full time to it and I really enjoy bringing the ideas I had in mind to live.
+
+            I like search  solutions to problems in the real world and the software is a powerful tool to change the paradigms of people
+          </p>
+          <Button>Look My Cv</Button>
+        </Col>  
+      </Row>
       </div>
-    </section>
+    </Container>
   )
 }
 
