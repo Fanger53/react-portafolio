64b7b9c543c93e60b992270e0a3c1c46b3e60253
Fanger53 <fanger53@gmail.com>
2023-02-06 22:24:43 -0500

    add layout and style to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import './home.scss';
 import {BsFillArrowRightSquareFill} from 'react-icons/bs';
+import {Button, Col, Container, Row} from 'react-bootstrap';
+import ReactTyped from 'react-typed';
+
 
 
 const Home = () => {
   return (
-    <section className='border d-flex justify-content-center home__card'>
-      <div className='border card'>
-        <h2>Leonardo Castillo</h2>
-        <p>Im a Developer tipyning script </p>
-        <button>know More <BsFillArrowRightSquareFill/></button>
-      </div>  
+    <section id='home'>
+      <Container className=' d-flex justify-content-center home__cards'>
+        <Row className='align-content-center'>
+          <Col xs={12}  className=' card'>
+            <h2>Leonardo Castillo</h2>
+            <p>Im  
+              <ReactTyped
+                loop
+                typeSpeed={130}
+                backSpeed={80}
+                strings={[' a Full-Stack Developer', ' a Freelancer', ' a Code Lover', ' a Mountain Biker', ' Leo']}
+                smartBackspace
+                shuffle={false}
+                backDelay={1}
+                fadeOut={false}
+                fadeOutDelay={100}
+                loopCount={0}
+                showCursor
+                cursorChar="|"
+              />
+            </p>
+            <Button variant={'transparent'} style={{backgroundColor: "transparent", color: "whitesmoke"}} >
+              know More <BsFillArrowRightSquareFill/>
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </section>
   )
 }
