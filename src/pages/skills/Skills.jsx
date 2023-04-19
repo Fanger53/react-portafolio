0a138aff77eb1168265228c138b2864031f513a7
Fanger53 <fanger53@gmail.com>
2023-04-19 11:33:23 -0500

    skill page configuration

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -5,8 +5,6 @@ import 'react-multi-carousel/lib/styles.css';
 import meter1 from '../../assets/img/meter1.svg';
 import meter2 from '../../assets/img/meter2.svg';
 import meter3 from '../../assets/img/meter3.svg';
-// import arrow1 from "../assets/img/arrow1.svg";
-// import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../../assets/img/color-sharp.png"
 
 const Skills = () => {
@@ -35,25 +33,25 @@ const Skills = () => {
         <div className="container">
             <div className="row">
                 <div className="col-12  align-content-center">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
+                    <div className=" skill-bx ">
+                        <h2 className='text-white'>Skills</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider ">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5 className='text-white'>Web Development</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5 className='text-white'>Brand Identity</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5 className='text-white'>Logo Design</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5 className='text-white'>Web Development</h5>
                             </div>
                         </Carousel>
                     </div>
